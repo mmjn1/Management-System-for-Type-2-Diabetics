774e3a99300711d4e6b0ce0a77c05fe1d3adf7ab
mmjn1 <mmjn1@student.le.ac.uk>
2024-01-18 17:46:53 +0000

    Configured the project in settings.py to use MySQL as the database. Environment variables have been added for security.

--- healthcare_project/healthcare_project/settings.py
+++ healthcare_project/healthcare_project/settings.py
@@ -106,8 +106,12 @@ WSGI_APPLICATION = 'healthcare_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
