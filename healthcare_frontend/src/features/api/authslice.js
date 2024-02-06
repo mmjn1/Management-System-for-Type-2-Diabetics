a1b1df8c64f5a365686029cd38a2859a3246d20d
mmjn1 <mmjn1@student.le.ac.uk>
2024-02-06 11:19:27 +0000

    Created RTK query API slice for contact form post request to the backend

--- healthcare_frontend/src/features/api/authslice.js
+++ healthcare_frontend/src/features/api/authslice.js
@@ -1,15 +1,19 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
-  reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/", // Django backend url
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api' }),
+  
+  endpoints: (builder) => ({
+    // Endpoint for submitting inquiry for the contact form on home page
+    submitInquiry: builder.mutation({
+      query: (inquiryData) => ({
+        url: 'create-contact/',
+        method: 'POST',
+        body: inquiryData,
+      }),
+    }),
   }),
-  // endpoints: (builder) => ({
-  //     registerPatient: builder.mutation({
-  //         query: (body) => ({
-  //             url: "register/patient/",
-  //             method: "POST",
-  //             body,
-  //         }),
 });
+
+export const { useSubmitInquiryMutation } = apiSlice;
