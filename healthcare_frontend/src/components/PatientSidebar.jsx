9bbd13ac80bbf62f5b98dcd291e7ab2fb76ddf29
Mukhtar Ngala <mmjn1@student.le.ac.uk>
2024-03-22 10:32:40 +0000

    Updated the sidebar's for patient and doctor to include the appointments along with the css design

--- healthcare_frontend/src/components/PatientSidebar.jsx
+++ healthcare_frontend/src/components/PatientSidebar.jsx
@@ -1,30 +1,54 @@
 import "../assets/patientcss/sidebar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import BloodtypeOutlinedIcon from '@mui/icons-material/BloodtypeOutlined';
-import AddTaskOutlinedIcon from '@mui/icons-material/AddTaskOutlined';
 import MedicationOutlinedIcon from '@mui/icons-material/MedicationOutlined';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
-import Diversity3OutlinedIcon from '@mui/icons-material/Diversity3Outlined';
 import DiningOutlinedIcon from '@mui/icons-material/DiningOutlined';
 import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import PaymentsOutlinedIcon from '@mui/icons-material/PaymentsOutlined';
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../features/api/userslice";
+import HomeOutlined from "@mui/icons-material/HomeOutlined";
+import { useEffect } from "react";
+import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
 
 
-const PatientSidebar = () =>{
+const PatientSidebar = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const loginstatus = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (loginstatus.token === null) {
+      const redirectPath = '/'
+      navigate(redirectPath);
+    }
+  }, [loginstatus, navigate]);
+  const logoutfun = () => {
+    dispatch(logout())
+  };
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
           <ul className="sidebarList">
+            <Link to="/" className="link">
+              <li className="sidebarListItem">
+                <HomeOutlined className="sidebarIcon" />
+                Home
+              </li>
+            </Link>
             <Link to="/patient/dashboard" className="link">
-            <li className="sidebarListItem active">
-              <DashboardOutlinedIcon className="sidebarIcon" />
-              Patient Dashboard
-            </li>
+              <li className={`sidebarListItem ${location.pathname === "/patient/dashboard" ? "active" : ""}`}>
+                <DashboardOutlinedIcon className="sidebarIcon" />
+                Patient Dashboard
+              </li>
             </Link>
 
           </ul>
@@ -32,24 +56,18 @@ const PatientSidebar = () =>{
         <div className="sidebarMenu">
           <h3 className="sidebarTitle"> Health Monitoring</h3>
           <ul className="sidebarList">
-            <Link to="/bloodsugar" className="link">
-              <li className="sidebarListItem">
+            <Link to="/patient/bloodsugar" className="link">
+              <li className={`sidebarListItem ${location.pathname === "/patient/bloodsugar" ? "active" : ""}`}>
                 <BloodtypeOutlinedIcon className="sidebarIcon" />
                 Blood Sugar Level
+              </li>
+            </Link>
 
-              </li>
-            </Link>
-            <Link to="/progresstracker" className="link">
+            <Link to="#prescriptions" className="link">
               <li className="sidebarListItem">
-                <AddTaskOutlinedIcon className="sidebarIcon" />
-                Progress Tracker
+                <MedicationOutlinedIcon className="sidebarIcon" />
+                My Prescriptions
               </li>
-            </Link>
-            <Link to="/prescriptions" className="link">
-            <li className="sidebarListItem">
-              <MedicationOutlinedIcon className="sidebarIcon" />
-                Prescriptions
-            </li>
             </Link>
           </ul>
         </div>
@@ -57,73 +75,77 @@ const PatientSidebar = () =>{
           <h3 className="sidebarTitle"> Consultations & Care</h3>
           <ul className="sidebarList">
 
-            <Link to="/appointments" className="link">
-            <li className="sidebarListItem">
-              <CalendarMonthOutlinedIcon className="sidebarIcon" />
-              Appointments
-            </li>
+            <Link to="/patient/appointments" className="link">
+              <li className={`sidebarListItem ${location.pathname === "/patient/appointments" ? "active" : ""}`}>
+                <CalendarMonthOutlinedIcon className="sidebarIcon" />
+                Appointments
+              </li>
+            </Link>
+
+            <Link to="/patient/chat" className="link">
+              <li className={`sidebarListItem ${location.pathname === "/patient/chat" ? "active" : ""}`}>
+                <ChatOutlinedIcon className="sidebarIcon" />
+                Chat with Doctor
+              </li>
+            </Link>
+
+            <Link to="/" className="link">
+              <li className={`sidebarListItem ${location.pathname === "/" ? "active" : ""}`}>
+                <LibraryBooksOutlinedIcon className="sidebarIcon" />
+                My Records
+              </li>
+            </Link>
+
+            <Link to='/diet' className="link">
+              <li className={`sidebarListItem ${location.pathname === "/diet" ? "active" : ""}`}>
+                <DiningOutlinedIcon className="sidebarIcon" />
+                Dietary Habits
+              </li>
             </Link>
 
             <li className="sidebarListItem">
-              <ChatOutlinedIcon className="sidebarIcon" />
-              Chat with Doctor
-            </li>
-            <li className="sidebarListItem">
-              <Diversity3OutlinedIcon className="sidebarIcon" />
-              My Care Team
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle"> Nutrition & Diet</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <DiningOutlinedIcon className="sidebarIcon" />
-              Dietary Habits
-            </li>
-            <li className="sidebarListItem">
-              <LibraryBooksOutlinedIcon className="sidebarIcon" />
+              <LocalLibraryOutlinedIcon className="sidebarIcon" />
               Educational Resources
             </li>
+
           </ul>
         </div>
+
         <div className="sidebarMenu">
           <h3 className="sidebarTitle"> Personal Account</h3>
           <ul className="sidebarList">
 
             <Link to="/patient/profile" className="link">
-            <li className="sidebarListItem">
-              <AccountCircleOutlinedIcon className="sidebarIcon" />
-              Profile
-            </li>
+              <li className={`sidebarListItem ${location.pathname === "/patient/profile" ? "active" : ""}`}>
+                <AccountCircleOutlinedIcon className="sidebarIcon" />
+                Profile
+              </li>
             </Link>
 
             <Link to="/payments" className="link">
-            <li className="sidebarListItem">
-              <PaymentsOutlinedIcon className="sidebarIcon" />
-              Payments
-            </li>
+              <li className={`sidebarListItem ${location.pathname === "/payments" ? "active" : ""}`}>
+                <PaymentsOutlinedIcon className="sidebarIcon" />
+                Payments
+              </li>
             </Link>
 
-          
+
           </ul>
         </div>
-
         <div className="sidebarMenu">
           <h3 className="sidebarTitle"> System</h3>
           <ul className="sidebarList">
 
-          <Link to="/" className="link">
-            <li className="sidebarListItem">
-              <LogoutOutlinedIcon className="sidebarIcon" />
-              Logout
-            </li>
+            <Link onClick={() => logoutfun()} className="link">
+              <li className="sidebarListItem">
+                <LogoutOutlinedIcon className="sidebarIcon" />
+                Logout
+              </li>
             </Link>
 
-         
+
           </ul>
         </div>
-        
       </div>
     </div>
   );
--- healthcare_frontend/src/components/SidebarDoctor.jsx
+++ healthcare_frontend/src/components/SidebarDoctor.jsx
@@ -1,26 +1,51 @@
 import "../assets/patientcss/sidebar.css";
-import { Link } from "react-router-dom";
-import DashboardIcon from '@mui/icons-material/DashboardOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import ChatIcon from '@mui/icons-material/ChatOutlined';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
-import Diversity3OutlinedIcon from '@mui/icons-material/Diversity3Outlined';
-import MedicationOutlinedIcon from '@mui/icons-material/MedicationOutlined';
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import DashboardIcon from "@mui/icons-material/DashboardOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import ChatIcon from "@mui/icons-material/ChatOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import MedicationOutlinedIcon from "@mui/icons-material/MedicationOutlined";
+import HomeOutlined from "@mui/icons-material/HomeOutlined";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../features/api/userslice";
+import { useEffect } from "react";
+import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
+import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
 
+const SidebarDoctor = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const loginstatus = useSelector((state) => state.user);
 
-const SidebarDoctor = () =>{
+  useEffect(() => {
+    if (loginstatus.token === null) {
+      const redirectPath = "/";
+      navigate(redirectPath);
+    }
+  }, [loginstatus, navigate]);
+  const logoutfun = () => {
+    dispatch(logout());
+  };
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <div className="sidebarMenu">
+          <h3 className="sidebarTitle"> Dashboard Overview </h3>
+
           <ul className="sidebarList">
+            <Link to="/" className="link">
+              <li className="sidebarListItem">
+                <HomeOutlined className="sidebarIcon" />
+                Home
+              </li>
+            </Link>
             <Link to="/doctor/dashboard" className="link">
-            <li className="sidebarListItem active">
-              <DashboardIcon className="sidebarIcon" />
-              Doctor Dashboard
-            </li>
+              <li className={`sidebarListItem ${location.pathname === "/doctor/dashboard" ? "active" : ""}`}>
+                <DashboardIcon className="sidebarIcon" />
+                Doctor Dashboard
+              </li>
             </Link>
           </ul>
         </div>
@@ -30,22 +55,16 @@ const SidebarDoctor = () =>{
           <ul className="sidebarList">
             <Link to="/" className="link">
               <li className="sidebarListItem">
-                <ListOutlinedIcon className="sidebarIcon" />
+                <AssignmentOutlinedIcon className="sidebarIcon" />
                 Patient Records
               </li>
             </Link>
-          </ul>
-        </div>
-
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle"> Appointments</h3>
-          <ul className="sidebarList">
 
             <Link to="/doctor/appointments" className="link">
-            <li className="sidebarListItem">
-              <CalendarMonthOutlinedIcon className="sidebarIcon" />
-              Appointment Calendar
-            </li>
+              <li className={`sidebarListItem ${location.pathname === "/doctor/appointments" ? "active" : ""}`}>
+                <CalendarMonthOutlinedIcon className="sidebarIcon" />
+                Appointment Calendar
+              </li>
             </Link>
           </ul>
         </div>
@@ -53,40 +72,29 @@ const SidebarDoctor = () =>{
         <div className="sidebarMenu">
           <h3 className="sidebarTitle"> Communications</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <ChatIcon className="sidebarIcon" />
-              Messages
-            </li>
+            <Link to="/doctor/chat" className="link">
+              <li className={`sidebarListItem ${location.pathname === "/doctor/chat" ? "active" : ""}`}>
+                <ChatIcon className="sidebarIcon" />
+                Messages
+              </li>
+            </Link>
           </ul>
         </div>
 
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle"> Care Management</h3>
-          <ul className="sidebarList">
-            <Link to="/" className="link">
-            <li className="sidebarListItem">
-              <Diversity3OutlinedIcon className="sidebarIcon" />
-              My Care Team
-            </li>
-            </Link>
-
-            <Link to="/" className="link">
-            <li className="sidebarListItem">
-              <MedicationOutlinedIcon className="sidebarIcon" />
-              Surgery Information
-            </li>
-            </Link>
-          </ul>
-
-        </div>
 
         <div className="sidebarMenu">
-          <h3 className="sidebarTitle"> Medications</h3>
+          <h3 className="sidebarTitle"> Medical Management </h3>
           <ul className="sidebarList">
             <li className="sidebarListItem">
               <MedicationOutlinedIcon className="sidebarIcon" />
-              Prescriptions Management
+              Prescription Requests
             </li>
+
+            <li className="sidebarListItem">
+              <LocalLibraryOutlinedIcon className="sidebarIcon" />
+              Medication Library
+            </li>
+
           </ul>
         </div>
 
@@ -94,28 +102,23 @@ const SidebarDoctor = () =>{
           <h3 className="sidebarTitle"> Personal Account </h3>
           <ul className="sidebarList">
             <Link to="/doctor/profile" className="link">
-            <li className="sidebarListItem">
-              <AccountCircleOutlinedIcon className="sidebarIcon" />
-              Profile
-            </li>
+              <li className={`sidebarListItem ${location.pathname === "/doctor/profile" ? "active" : ""}`}>
+                <AccountCircleOutlinedIcon className="sidebarIcon" />
+                Profile
+              </li>
             </Link>
-          </ul>
-        </div>
 
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle"> System </h3>
-          <ul className="sidebarList">
-            <Link to="/" className="link">
-            <li className="sidebarListItem">
-              <LogoutOutlinedIcon className="sidebarIcon" />
-              Logout
-            </li>
+            <Link onClick={() => logoutfun()} className="link">
+              <li className="sidebarListItem">
+                <LogoutOutlinedIcon className="sidebarIcon" />
+                Logout
+              </li>
             </Link>
           </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default SidebarDoctor;
