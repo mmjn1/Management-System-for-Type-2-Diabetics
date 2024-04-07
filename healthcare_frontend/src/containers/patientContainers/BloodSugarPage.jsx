dd8beadcab519f9ed1d221bbda7291850abd3bb1
Mukhtar Ngala <mmjn1@student.le.ac.uk>
2024-04-07 15:16:15 +0100

    Added new dependencies and updated redux store

--- healthcare_frontend/src/containers/patientContainers/BloodSugarPage.jsx
+++ healthcare_frontend/src/containers/patientContainers/BloodSugarPage.jsx
@@ -1,18 +1,113 @@
-import React from 'react';
-import Sidebar from '../../components/PatientSidebar';
-import Navbar from '../../components/PostLoginNavigation';
+import React, {useEffect, useState} from 'react';
 import "../../sass/PatientDashboard.scss";
-import { Link } from 'react-router-dom';
+import {Doughnut} from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import {useDispatch, useSelector} from "react-redux";
+import {userDetails} from "../../features/api/Userdetails";
+
 
 const BloodSugarPage = () => {
-    return (   
-        <div className="home">
-            <Sidebar />
+    const dispatch = useDispatch()
+    const details = useSelector((state) => state.userDetails)
+    useEffect(() => {
+        dispatch(userDetails())
+    }, []);
+    useEffect(() => {
+        if (details.status === 'succeeded') {
+            const centerTextPlugin = {
+                id: 'centerText',
+                afterDraw: (chart) => {
+                    const ctx = chart.ctx;
+                    const width = chart.width;
+                    const height = chart.height;
+                    ctx.restore();
+                    let fontSize = (height / 100).toFixed(2);
+                    ctx.font = `${fontSize}em sans-serif`;
+                    ctx.textBaseline = 'middle';
+
+                    const text = details.data.information.blood_sugar_level, 
+                        textX = Math.round((width - ctx.measureText(text).width) / 2),
+                        textY = height / 2;
+
+                    ctx.fillText(text, textX, textY);
+                    ctx.save();
+                }
+            };
+
+
+            Chart.register(centerTextPlugin);
+        }
+
+    }, [details.status]);
+    const data = {
+        datasets: [
+            {
+                data: [100],
+                backgroundColor: ['#1BC5BD'],
+                hoverBackgroundColor: ['#C9f7f5'],
+                borderRadius: 20, 
+
+            },
+        ],
+        labels: ['Level'],
+    };
+
+    const options = {
+        cutout: '80%',
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                enabled: true,
+            }
+        },
+    };
+    return (
+        <div className="home container">
             <div className="homeContainer">
-                <Navbar />
                 <div className="content">
-                    {/* Your blood sugar page content goes here */}
-                    Blood Sugar Page
+                    {details.status === 'succeeded' ?
+                        <div className='row'>
+                            <div className="col-lg-6">
+                                {/*begin::Mixed Widget 14*/}
+                                <div className="card card-custom card-stretch gutter-b">
+                                    {/*begin::Header*/}
+                                    <div className="card-header border-0 pt-5">
+                                        <h3 className="card-title font-weight-bolder">Blood Sugar</h3>
+                                        <div className="card-toolbar">
+
+                                        </div>
+                                    </div>
+                                   
+                                    <div className="card-body d-flex flex-column">
+                                        <div className="flex-grow-1">
+                                            <div style={{
+                                                height: '250px',
+                                                width: '100%',
+                                                display: 'flex',
+                                                justifyContent: 'center', // Center horizontally
+                                                alignItems: 'center', // Center vertically
+                                                position: 'relative',
+                                            }}>
+                                                <Doughnut redraw={true}
+                                                          data={data} options={options}/>
+                                            </div>
+                                            {/*<div id="kt_mixed_widget_14_chart" style={{height: '200px'}}/>*/}
+                                        </div>
+                                        <div className="pt-5">
+                                            <p className="text-center font-weight-normal font-size-lg pb-7">Targeted:<b>
+                                                {details.data.information.target_blood_sugar_level}
+                                            </b> mg/dL</p>
+                                            <a href="#"
+                                               className="btn btn-success btn-shadow-hover font-weight-bolder w-100 py-3">{details.data.information.type_of_diabetes}</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                        </div> : 'loading'}
 
                 </div>
             </div>
--- healthcare_frontend/src/store.js
+++ healthcare_frontend/src/store.js
@@ -1,22 +1,25 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from './features/api/userslice';
-import registerDoctor from "./features/doctor_register";
-import registerUserPatient from "./features/patient_register";
+import registerDoctor from "./features/doctor/doctor_register";
+import registerUserPatient from "./features/patient/patient_register";
 import forgetPasswordfunction from "./features/api/forgetPassword";
 import ForgetPasswordConfirm from "./features/api/forgetPassword_confirm";
-import fetchDoctor from "./features/FetchDoctor";
+import fetchDoctor from "./features/doctor/FetchDoctor";
 import userDetailsSlice from "./features/api/Userdetails";
-import { apiSlice } from "../src/features/api/authslice";
-import appointmentsReducer from "./features/api/appointmentsSlice";
-import { patient_appointment } from "./features/api/create_appointment";
+import { apiSlice } from "./features/api/contactSlice";
+import appointmentsReducer from "./features/appointments/appointmentsSlice";
+import { patientAppointmentCreation } from "./features/appointments/patientAppointment";
 import chatReducer from "./features/chat/chatSlice";
-
-
+import fetchPatient from "./features/patient/fetchPatients";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { persistStore, persistReducer } from "redux-persist";
-import { doctorApi } from './features/availabilitySlice';
-
+import { doctorApi } from './features/appointments/availabilitySlice';
+import { updateProfile } from './features/patient/updateProfile';
+import { doctorAvailabilityApi } from './features/appointments/doctorAvailabilitySlice';
+import { timeslotApi } from './features/timeslotAPI';
+import patientRecordsReducer from "./features/api/patient_records";
+import { updateDoctorProfile } from './features/doctor/updateDoctorProfile';
 
 const persistConfig = {
   key: 'root',
@@ -27,7 +30,12 @@ const persistConfig = {
 const rootReducer = combineReducers({
   [apiSlice.reducerPath]: apiSlice.reducer,
   [doctorApi.reducerPath]: doctorApi.reducer,
-
+  [updateProfile.reducerPath]: updateProfile.reducer,
+  [doctorAvailabilityApi.reducerPath]: doctorAvailabilityApi.reducer,
+  [patientAppointmentCreation.reducerPath]: patientAppointmentCreation.reducer,
+  [timeslotApi.reducerPath]: timeslotApi.reducer,
+  [updateDoctorProfile.reducerPath]: updateDoctorProfile.reducer, // Update this line to match the new reducerPath
+  
   user: userReducer,
   registerDoctor: registerDoctor,
   registerUserPatient: registerUserPatient,
@@ -36,23 +44,27 @@ const rootReducer = combineReducers({
   DoctorSlice: fetchDoctor,
   userDetails: userDetailsSlice,
   appointments: appointmentsReducer,
-  patientAppointment: patient_appointment,
   chat: chatReducer,
+  PatientSlice: fetchPatient,
+  patientRecords: patientRecordsReducer, 
+
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: persistedReducer, 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/PURGE'],
       },
-    }).concat(doctorApi.middleware),
+    }).concat(doctorApi.middleware).concat(doctorAvailabilityApi.middleware)
+      .concat(patientAppointmentCreation.middleware).concat(timeslotApi.middleware)
+      .concat(updateProfile.middleware).concat(updateDoctorProfile.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-export const persistor = persistStore(store); 
+export const persistor = persistStore(store);
 
 export default { store, persistor };
