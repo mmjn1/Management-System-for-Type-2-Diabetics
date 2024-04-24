4a5fb864ed9aeae591a21b51f208b7d32441b0c3
Mukhtar Ngala <mmjn1@student.le.ac.uk>
2024-04-24 09:41:42 +0100

    Added BlackMedicine and BlankPatient components

---
+++ healthcare_frontend/src/containers/doctorContainers/BlackMedicine.js
@@ -0,0 +1,42 @@
+const BlackMedicine = () => {
+    return (
+      <div className='MuiBox-root css-0'>
+        <span
+          className='MuiSkeleton-root MuiSkeleton-rounded css-5kwfos'
+          style={{ width: '50%', height: '15px' }}
+        />
+        <div className='css-kmutc5'>
+          <span
+            className='MuiSkeleton-root MuiSkeleton-rectangular css-1ja6z00'
+            style={{
+              width: '15px',
+              height: '15px',
+            }}
+          />
+          <span
+            className='MuiSkeleton-root MuiSkeleton-rectangular css-1ja6z00'
+            style={{
+              width: '15px',
+              height: '15px',
+            }}
+          />
+          <span
+            className='MuiSkeleton-root MuiSkeleton-rectangular css-1ja6z00'
+            style={{
+              width: '15px',
+              height: '15px',
+            }}
+          />
+          <span
+            className='MuiSkeleton-root MuiSkeleton-rounded css-2igmjn'
+            style={{ width: '20%', height: '10px' }}
+          />
+          <span
+            className='MuiSkeleton-root MuiSkeleton-rounded css-2igmjn'
+            style={{ width: '10%', height: '10px' }}
+          />
+        </div>
+      </div>
+    )
+  }
+  export default BlackMedicine
---
+++ healthcare_frontend/src/containers/doctorContainers/BlankPatient.js
@@ -0,0 +1,35 @@
+
+const BlankPatient = () => {
+    return (
+      <div className='black-patient-outer'>
+        <div className='row'>
+          <div className='col-3 px-1 m-0'>
+            <span
+              className='black-patient-inner'
+              style={{ width: '100px', height: '25px' }}
+            />
+          </div>
+          <div className='col-3 px-1 m-0'>
+            <span
+              className='black-patient-inner'
+              style={{ width: '100px', height: '25px' }}
+            />
+          </div>
+          <div className='col-3 px-1 m-0'>
+            <span
+              className='black-patient-inner'
+              style={{ width: '100px', height: '25px' }}
+            />
+          </div>
+          <div className='col-3 px-1 m-0'>
+            <span
+              className='black-patient-inner'
+              style={{ width: '100px', height: '25px' }}
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+  export default BlankPatient
+  
