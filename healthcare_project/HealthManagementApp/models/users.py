e5941149b64e3f659bce71c86285fc4c3ed7d962
Mukhtar Ngala <mmjn1@student.le.ac.uk>
2024-04-29 20:52:36 +0100

    Updated doctor-patient relationship in models

--- healthcare_project/HealthManagementApp/models/users.py
+++ healthcare_project/HealthManagementApp/models/users.py
@@ -87,7 +87,7 @@ class CustomUser(AbstractUser, PermissionsMixin):
 
 class Doctor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="doctor_user")
-    patients = models.ManyToManyField('HealthManagementApp.Patient', related_name='treated_by_doctors', null=True,
+    patients = models.ManyToManyField('HealthManagementApp.Patient', related_name='treated_by_doctors',
                                       blank=True)
     speciality = models.CharField(max_length=50, blank=True, null=True)
     years_of_experience = models.CharField(max_length=50, blank=True, null=True)
